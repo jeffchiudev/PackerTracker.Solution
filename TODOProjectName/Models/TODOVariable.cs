af25a1133760f8f9607dcd7d1d87eb0c98474381
jeffchiudev <jwchiu82@gmail.com>
2020-12-16 13:53:03 -0800

    Initial commit

---
+++ TODOProjectName/Models/TODOVariable.cs
@@ -0,0 +1,8 @@
+namespace TODOProjectName.Models
+{
+    public class InputVariable //public constructor for your variables below
+    {
+        public string Country { get; set; } //public variables
+        //methods as well
+    }
+}
